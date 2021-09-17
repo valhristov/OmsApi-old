e0cb8e27c36685a46221f8da8d3ab165f81c00ca
Valeri Hristov <valeri.hristov@inexto.com>
2021-09-17 12:40:33 +0200

    2021-09-17 - RU: adding more report statuses + removed numbers from operationId fields

--- FixJson/FixJson/Program.cs
+++ FixJson/FixJson/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
@@ -9,6 +10,7 @@ namespace FixJson
     class Program
     {
         private static readonly Regex _apiExamplesFixer = new("(\"example\"\\:\\s*?\\[)(?<wrong>['\\w,\\s]+)+(\\])");
+        private static readonly Regex _apiOperationIdFixer = new("(\"operationId\"\\:\\s*?\"\\w+?)(_\\d+?)(\",)");
 
         static void Main(string[] args)
         {
@@ -22,15 +24,27 @@ namespace FixJson
 
             var content = File.ReadAllText(path);
 
-            var json = Fix(content);
+            // Debugger.Launch();
 
+            var json = FixExamples(content);
+            json = FixOperationIds(json);
+
+            if (json.Contains("GET_"))
+            {
+                Debugger.Launch();
+            }
             var document = JsonSerializer.Deserialize<object>(json);
+            var indented = JsonSerializer.Serialize(document, new JsonSerializerOptions { WriteIndented = true });
 
-            File.WriteAllText(path, JsonSerializer.Serialize(document, new JsonSerializerOptions { WriteIndented = true }));
+            File.WriteAllText(path, indented);
         }
 
-        private static string Fix(string json) =>
+        private static string FixExamples(string json) =>
             // Some field examples have invalid JSON, this removes them
             _apiExamplesFixer.Replace(json, "\"example\":[]");
+
+        private static string FixOperationIds(string json) =>
+            // Some field examples have invalid JSON, this removes them
+            _apiOperationIdFixer.Replace(json, "$1$3");
     }
 }
