854b922c2cd8a4be33fc26a5dc34001748780eff
Valeri Hristov <valeri.hristov@inexto.com>
2022-01-21 16:47:41 +0100

    2022-01-21 - UZ: added

--- run.bat
+++ run.bat
@@ -7,6 +7,9 @@ curl https://suzcloud.stage.ismet.kz/v2/api-docs > kz.json
 curl https://oms.megacom.kg/v2/api-docs > kg.json
 .\FixJson\FixJson\bin\Debug\net5.0\FixJson.exe kg.json
 
+curl https://omscloud.asllikbelgisi.uz/v2/api-docs > uz.json
+.\FixJson\FixJson\bin\Debug\net5.0\FixJson.exe uz.json
+
 set %date% = date /t
 git add .
 git commit -m %date%
